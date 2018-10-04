673bd227ab7b54578af012c330cc7b256975be71
Praveen Vikram <praveen.vikram@kiosa.org>
2018-10-03 21:39:36 -0400

    refactor: move helper fns to helper.clj

---
+++ src/clj/reddit_trac/helper.clj
@@ -0,0 +1,16 @@
+(ns reddit-trac.helper)
+
+(defn as-int [int-str]
+  (java.lang.Integer/parseInt int-str))
+
+(defn as-float [float-str]
+  (java.lang.Float/parseFloat float-str))
+
+(defn as-bool [bool-str]
+  (or (#{"true"} bool-str) false))
+
+(defn map-with-keys [map keys-vec]
+  (if keys-vec
+    (select-keys map keys-vec)
+    map))
+
--- src/clj/reddit_trac/routes.clj
+++ src/clj/reddit_trac/routes.clj
@@ -3,30 +3,25 @@
             [compojure.route    :refer [not-found files resources]]
             [ring.util.response :refer [response]]
             [taoensso.timbre    :as    log]
-            [reddit-trac.api    :as    api]))
-
-(defn as-int [int-str]
-  (java.lang.Integer/parseInt int-str))
-
-(defn as-bool [bool-str]
-  (or (#{"true"} bool-str) false))
+            [reddit-trac.api    :as    api]
+            [reddit-trac.helper :as    h]))
 
 (defroutes api-routes
   (context "/api" {body :body}
            ;; CREATE
            (PUT "/watch" [] (api/create-watch body))
            (GET "/watch/validate/:id{[0-9]+}"
-                [token email id :<< as-int]
+                [token email id :<< h/as-int]
                 (api/validate-watch id email token))
            ;; READ
            (GET "/watch" [token email]
                 (api/get-watch :email email token))
-           (GET "/watch/:id{[0-9]+}" [token id :<< as-int]
+           (GET "/watch/:id{[0-9]+}" [token id :<< h/as-int]
                 (api/get-watch :id id token))
            ;; UPDATE
            ;; (POST "/watch" [] (println "update"))
            ;; DELETE
-           (DELETE "/watch/:id{[0-9]+}" [token email id :<< as-int] (api/delete-watch id email token))))
+           (DELETE "/watch/:id{[0-9]+}" [token email id :<< h/as-int] (api/delete-watch id email token))))
 
 (defroutes gen-routes 
   (GET "/" [] "Hello from Compojure!")  ;; for testing only
